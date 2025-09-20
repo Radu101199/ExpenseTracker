9237005ae43b2ae305bc6aa4de5950b8d3702973
PAUN RADU <raducu.paun@gmail.com>
2025-09-20 18:24:06 +0300

    first week backend

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b4c03b3d1135e7d32474070b9e87bd95f692c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb32ae2ea3d9be261ad14f8f66826ec27d0dc4c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
